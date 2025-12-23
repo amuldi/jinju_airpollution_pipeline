fee3027b1f48245bf256ab9ca1f0f389963b3e3a
Sihun <88440385+amuldi@users.noreply.github.com>
2025-12-23 16:28:50 +0900

    Add NOT NULL constraint and created_at column

--- ddl/stg_airpollution_jinju.sql
+++ ddl/stg_airpollution_jinju.sql
@@ -1,14 +1,19 @@
-
 CREATE TABLE stg_airpollution_jinju (
-  measurement_time DATETIME,
-  pm25 DECIMAL(6,2),
-  pm10 DECIMAL(6,2),
-  no2 DECIMAL(6,4),
-  o3 DECIMAL(6,4),
-  co DECIMAL(6,4),
-  so2 DECIMAL(6,4),
-  addr VARCHAR(255),
-  latitude DECIMAL(10,6),
-  longitude DECIMAL(10,6),
-  data_quality_flag VARCHAR(20)
+    measurement_time DATETIME NOT NULL,
+
+    pm25   DECIMAL(6,2),
+    pm10   DECIMAL(6,2),
+    no2    DECIMAL(6,4),
+    o3     DECIMAL(6,4),
+    co     DECIMAL(6,4),
+    so2    DECIMAL(6,4),
+
+    addr        VARCHAR(255),
+    latitude    DECIMAL(10,6),
+    longitude   DECIMAL(10,6),
+
+    data_quality_flag VARCHAR(20),
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (measurement_time, addr)
 );
